ec1c53e0cb4eafa710c2fb33d2b86cd366f39183
SoulSlyde <soulslyde@gmail.com>
2026-06-22 04:10:03 +0200

    Interaction with NPC
    Added NPC interact icons that show you can talk to him and the interaction button appear when the player is close at range 2.

---
+++ Assets/OMAR/DialogSystem/NPCInteractionIcon.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class NPCInteractionIcon : MonoBehaviour
+{
+    public GameObject iconFar;//icon far
+    public GameObject iconNear;//icon if the player is near
+
+    public float detectionRange = 4f;//distance detection for the player
+    public float talkRange = 2f;//distance to talk
+
+    Transform player;
+    DialogueUI dialogueUI;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        //fix dialogue ui finding
+        dialogueUI = FindAnyObjectByType<DialogueUI>(FindObjectsInactive.Include);
+
+        iconFar.SetActive(true);
+        iconNear.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (player == null) return;
+
+        float dist = Vector3.Distance(player.position, transform.position);
+
+        //if dialogue is open don't show the icons
+        if (dialogueUI != null && (dialogueUI.IsOpen || dialogueUI.OptionsActive))
+        {
+            iconFar.SetActive(false);
+            iconNear.SetActive(false);
+            return;
+        }
+
+        //show far icon at distance
+        if (dist > detectionRange)
+        {
+            iconFar.SetActive(true);
+            iconNear.SetActive(false);
+            return;
+        }
+
+        
+        if (dist <= detectionRange && dist > talkRange)
+        {
+            iconFar.SetActive(true);
+            iconNear.SetActive(false);
+            return;
+        }
+
+       
+        if (dist <= talkRange)
+        {
+            iconFar.SetActive(false);
+            iconNear.SetActive(true);
+        }
+    }
+}
